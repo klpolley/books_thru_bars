a96d16ddf0a07eb0c52c11930f9f7a8f3993d902
Kate Polley <kpolley@ithaca.edu>
2020-02-02 21:50:38 -0500

    import into book table

---
+++ data_import/import.py
@@ -0,0 +1,52 @@
+import sys
+from psycopg2 import Error, DatabaseError
+
+from data_import.connect import connect, disconnect
+from data_import.read_csv import read_library_csv
+
+
+def insert_books(conn, values):
+
+    titles_seen = set()
+
+    for row in values:
+        try:
+            c = conn.cursor()
+
+            if row['title'] in titles_seen:
+                continue
+
+            select = "SELECT genreID from genre where name ="+"'"+row['genre']+"'"
+            c.execute(select)
+            data = c.fetchall()
+            genreID = data[0][0]
+
+            insert = "INSERT INTO book(title, genreID) VALUES(" + "'" + row['title'] + "'" + "," + str(genreID) + ")"
+            c.execute(insert)
+
+            titles_seen.add(row['title'])
+
+        except Error as e:
+            print(e)
+
+
+
+def import_library():
+    conn = connect()
+
+    try:
+        values = read_library_csv('catalog.csv')
+        #insert_books(conn, values)
+
+        conn.commit()
+
+    except DatabaseError as e:
+        print(f'Error {e}')
+        sys.exit(1)
+
+    disconnect(conn)
+
+
+
+if __name__ == '__main__':
+    import_library()
