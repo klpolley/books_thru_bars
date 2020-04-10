41c93c9bcbf9639ad01ad6e3bcae0c2f6dd494b4
Kate Polley <kpolley@ithaca.edu>
2020-04-10 13:16:50 -0400

    security update

--- app/__init__.py
+++ app/__init__.py
@@ -13,17 +13,7 @@ login = LoginManager(app)
 bcrypt = Bcrypt(app)
 csrf = CSRFProtect(app)
 
-app.secret_key = 'BTB key'
-
-app.config["MAIL_SERVER"] = "smtp.googlemail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = 1
-app.config["MAIL_USERNAME"] = 'booksthrubars@gmail.com'
-app.config['MAIL_DEFAULT_SENDER'] = 'booksthrubars@gmail.com'
-app.config["MAIL_PASSWORD"] = 'angler$$'
-
 mail = Mail(app)
-
 
 from app import routes, errors
 
--- config.py
+++ config.py
@@ -3,4 +3,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-a-very-secret-key'
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_DEFAULT_SENDER = MAIL_USERNAME
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = [MAIL_USERNAME]
